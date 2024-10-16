06c9089bdf0913ab80d0a5799b170843ad6a19ea
Giile <126676548+Giile@users.noreply.github.com>
2024-10-16 12:09:02 -0300

    commit

--- models/Pessoa.php
+++ models/Pessoa.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once './models/ConexaoBanco.php';
+require_once "./models/ConexaoBanco.php";
 
-class Pessoa extends ConexaoBanco { 
+class Pessoa extends ConexaoBanco {
     function __construct(){
         parent::__construct();
     }
@@ -10,6 +10,19 @@ class Pessoa extends ConexaoBanco {
     function selectAll() {
         $sql = $this->conBD->prepare("SELECT * FROM pessoa ORDER BY nome");
         $sql->execute();
-        return $sql->fetchAll();        
+        return $sql->fetchAll();    
+    }
+
+    function insert($data) {
+        $sql = $this->conBD->prepare("INSERT INTO pessoa (Nome, CPF, Idade, Telefone, Email, Endereco, Observacao) VALUES (:Nome, :CPF, :Idade, :Telefone, :Email, :Endereco, :Observacao)");
+        $sql->bindParam(":Nome", $data["Nome"]);
+        $sql->bindParam(":CPF", $data["CPF"]);
+        $sql->bindParam(":Idade", $data["Idade"]);
+        $sql->bindParam(":Telefone", $data["Telefone"]);
+        $sql->bindParam(":Email", $data["Email"]);
+        $sql->bindParam(":Endereco", $data["Endereco"]);
+        $sql->bindParam(":Observacao", $data["Observacao"]); 
+        $sql->execute();
+        return $sql->rowCount();
     }
 }
